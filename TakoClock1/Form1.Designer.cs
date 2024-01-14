c07f8e5b3e259f1446cbe91ceff35d52f00cc089
klas9802 <klas9806@gmail.com>
2024-01-14 17:19:35 +0800

    v2.0

---
+++ TakoClock1/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace TakoClock1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.labelCurrentTime = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.inputLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // labelCurrentTime
+            // 
+            this.labelCurrentTime.AutoSize = true;
+            this.labelCurrentTime.BackColor = System.Drawing.Color.White;
+            this.labelCurrentTime.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.labelCurrentTime.Location = new System.Drawing.Point(28, 22);
+            this.labelCurrentTime.Name = "labelCurrentTime";
+            this.labelCurrentTime.Size = new System.Drawing.Size(126, 24);
+            this.labelCurrentTime.TabIndex = 0;
+            this.labelCurrentTime.Text = "CurrentTime";
+            this.labelCurrentTime.Click += new System.EventHandler(this.labelCurrentTime_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.textBox1.Location = new System.Drawing.Point(32, 52);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(173, 29);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "00:00";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button1.Location = new System.Drawing.Point(32, 87);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(95, 41);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "設定鬧鐘";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // inputLabel
+            // 
+            this.inputLabel.AutoSize = true;
+            this.inputLabel.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.inputLabel.Location = new System.Drawing.Point(28, 147);
+            this.inputLabel.Name = "inputLabel";
+            this.inputLabel.Size = new System.Drawing.Size(138, 21);
+            this.inputLabel.TabIndex = 3;
+            this.inputLabel.Text = "目前鬧鐘設定為空";
+            this.inputLabel.Click += new System.EventHandler(this.inputLabel_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(223, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 21);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "警告訊息";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::TakoClock1.Properties.Resources.Ld07cYy;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(510, 297);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.inputLabel);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.labelCurrentTime);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "TakoClock";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label labelCurrentTime;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label inputLabel;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ TakoClock1/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TakoClock1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            UpdateCurrentTime();
+        }
+
+        private void UpdateCurrentTime()
+        {
+            // 取得當前的日期和時間
+            DateTime currentTime = DateTime.Now;
+
+            // 將當前時間顯示在 Label 控制項上
+            labelCurrentTime.Text = currentTime.ToString("HH:mm:ss");
+        }
+
+        private void labelCurrentTime_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            UpdateCurrentTime();
+
+            if (DateTime.TryParse(textBox1.Text, out DateTime userInput))
+            {
+                DateTime currentTime = DateTime.Now;
+                string curTime = currentTime.ToString("HH:mm");
+                string userTime = userInput.ToString("HH:mm");
+
+                // 比對時間
+                if (curTime == userTime)
+                {
+                    string path = Environment.CurrentDirectory;
+                    string musicPath = path.Replace(@"\bin\Debug", @"\Audio\ina語音包.wav");
+                    try
+                    {
+                        this.timer1.Stop();
+                        SoundPlayer simpleSound = new SoundPlayer(musicPath);
+                        simpleSound.Play();
+                    }
+                    catch
+                    {
+                        label1.Text = "Not Find Ina";
+                        return;
+                    }
+                    
+                }
+            }
+            else
+            {
+                // 使用者輸入的不是有效的日期時間字串，你可以在這裡處理錯誤
+                //MessageBox.Show("請輸入有效的日期時間格式。");
+                label1.Text = "請輸入有效的日期時間格式。";
+                return;
+            }
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            inputLabel.Text = textBox1.Text;
+            timer1.Start();
+
+        }
+
+        private void inputLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
